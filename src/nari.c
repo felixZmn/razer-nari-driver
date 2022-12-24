f770227467a5fef29411f08da48c58dfdc829e0a
felixZmn <felix.zimmermann.1.de@gmail.com>
2022-12-24 14:03:52 +0100

    working endpoint for setting mute status

--- src/nari.c
+++ src/nari.c
@@ -7,9 +7,35 @@
 #include <linux/usb.h>
 
 void mute_mic(struct device *dev){
-// does nothing
+    struct usb_interface *intf = to_usb_interface(dev->parent);
+    struct usb_device *usbdev = interface_to_usbdev(intf);
+
+    int foo = 0x0001;
+
+    __u8 request = 1;
+    __u8 requesttype = 0x21;
+    __u16 value = 0x0100;
+    __u16 index = 0x0300;
+    //char *data = &foo;
+    __u16 size = 1;
+    int timeout = USB_CTRL_SET_TIMEOUT;
+
+    usb_control_msg(usbdev, usb_sndctrlpipe(usbdev, 0), request, requesttype, value, index, &foo, size, timeout);
 }
 
 void unmute_mic(struct device *dev){
-// does nothing
+    struct usb_interface *intf = to_usb_interface(dev->parent);
+    struct usb_device *usbdev = interface_to_usbdev(intf);
+
+    int foo = 0x0000;
+
+    __u8 request = 1;
+    __u8 requesttype = 0x21;
+    __u16 value = 0x0100;
+    __u16 index = 0x0300;
+    //char *data = &foo;
+    __u16 size = 1;
+    int timeout = USB_CTRL_SET_TIMEOUT;
+
+    usb_control_msg(usbdev, usb_sndctrlpipe(usbdev, 0), request, requesttype, value, index, &foo, size, timeout);
 }
